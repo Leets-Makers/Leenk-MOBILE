b18eabfb4540a5d6e975446368d23d9407ef25fe
JIN921 <wlslwlsl808@gmail.com>
2025-06-17 14:46:24 +0900

    feat: #3 부가 페이지 추가

--- app/(page)/private.tsx
+++ app/(page)/private.tsx
@@ -1,17 +1,51 @@
+import { View, Text, Pressable, StyleSheet, Image } from 'react-native';
 import colors from '@/theme/color';
-import { View, Text } from 'react-native';
+import LockIcon from '@/assets/images/ic_lock.svg';
 
 export default function PrivatePage() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: colors.bg[2],
-      }}
-    >
-      <Text>부가 페이지</Text>
+    <View style={styles.container}>
+      <LockIcon width={120} height={120} />
+
+      <Text style={styles.text}>재밌는 기능들을 준비중이야</Text>
+
+      <Pressable style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>이런 것도 있었으면 좋겠어</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.bg[2],
+    gap: 20,
+  },
+  image: {
+    width: 120,
+    height: 120,
+    marginBottom: 12,
+  },
+  text: {
+    fontWeight: '700',
+    fontFamily: 'NanumSquareNeo-Regular',
+    fontSize: 16,
+    lineHeight: 24,
+    color: colors.text[3],
+  },
+  buttonContainer: {
+    backgroundColor: colors.white,
+    borderRadius: 16,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+  },
+  buttonText: {
+    fontWeight: '700',
+    fontFamily: 'NanumSquareNeo-Light',
+    fontSize: 12,
+    color: colors.primary,
+  },
+});
