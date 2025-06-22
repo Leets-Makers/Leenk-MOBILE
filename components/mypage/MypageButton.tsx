59fd00110e0f19837cc276b55bd3973cc4f6f45b
JIN921 <wlslwlsl808@gmail.com>
2025-06-22 17:41:47 +0900

    refactor: #12 마이페이지 버튼 기능 추가

--- components/mypage/MypageButton.tsx
+++ components/mypage/MypageButton.tsx
@@ -3,18 +3,33 @@ import colors from '@/theme/color';
 import { fonts, fontSize, width } from '@/theme/globalStyles';
 import React from 'react';
 import styled from 'styled-components/native';
+import Toggle from '@/components/common/Toggle';
+
+type MyPageButtonType = 'arrow' | 'toggle' | 'none';
+
+interface MyPageButtonProps {
+  text: string;
+  type?: MyPageButtonType;
+  onPress?: () => void;
+  isToggleOn?: boolean;
+  onToggle?: () => void;
+}
 
 export default function MyPageButton({
   text,
+  type = 'arrow',
   onPress,
-}: {
-  text: string;
-  onPress: () => void;
-}) {
+  isToggleOn = false,
+  onToggle,
+}: MyPageButtonProps) {
   return (
-    <Container onPress={onPress}>
-      <Title>{text}</Title>
-      <RightArrowIcon />
+    <Container onPress={type === 'arrow' ? onPress : undefined}>
+      <Title onlyText={type === 'none'}>{text}</Title>
+
+      {type === 'arrow' && <RightArrowIcon />}
+      {type === 'toggle' && (
+        <Toggle isOn={isToggleOn} onToggle={onToggle ?? (() => {})} />
+      )}
     </Container>
   );
 }
@@ -29,9 +44,11 @@ const Container = styled.Pressable`
   align-items: center;
 `;
 
-const Title = styled.Text`
+const Title = styled.Text<{ onlyText: boolean }>`
   font-size: ${fontSize.md}px;
   font-weight: 700;
   color: ${colors.text[1]};
   font-family: ${fonts.Regular};
+  text-align: ${({ onlyText }) => (onlyText ? 'left' : 'auto')};
+  flex: 1;
 `;
