803510272f12305201d3cd2059e4b6d771b81dff
JIN921 <wlslwlsl808@gmail.com>
2025-06-22 17:50:37 +0900

    feat: #12 알림 설정 상태 관리

--- app/account/setting/notifications.tsx
+++ app/account/setting/notifications.tsx
@@ -10,11 +10,19 @@ import {
   width,
 } from '@/theme/globalStyles';
 import { useRouter } from 'expo-router';
+import { useState } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 
 export default function AccountStatusPage() {
   const router = useRouter();
+
+  // ✅ 상태 관리
+  const [feedLike, setFeedLike] = useState(false);
+  const [newLinkPost, setNewLinkPost] = useState(false);
+  const [newFeedPost, setNewFeedPost] = useState(false);
+  const [linkJoinRequest, setLinkJoinRequest] = useState(false);
+
   return (
     <Container>
       <Header
@@ -26,7 +34,7 @@ export default function AccountStatusPage() {
         TitleSection={
           <Text
             style={{
-              fontWeight: 700,
+              fontWeight: '700',
               fontSize: fontSize.lg,
               fontFamily: fonts.Regular,
               lineHeight: lineHeight.l,
@@ -39,32 +47,27 @@ export default function AccountStatusPage() {
       <MarginContainer>
         <MyPageButton
           text="피드 좋아요"
-          onPress={() => {
-            console.log('알림설정');
-          }}
           type="toggle"
+          isToggleOn={feedLike}
+          onToggle={() => setFeedLike((prev) => !prev)}
         />
         <MyPageButton
           text="링크 새 게시물"
-          onPress={() => {
-            console.log('링크 새 게시물');
-          }}
           type="toggle"
+          isToggleOn={newLinkPost}
+          onToggle={() => setNewLinkPost((prev) => !prev)}
         />
-
         <MyPageButton
           text="피드 새 게시물"
-          onPress={() => {
-            console.log('피드 새 게시물');
-          }}
           type="toggle"
+          isToggleOn={newFeedPost}
+          onToggle={() => setNewFeedPost((prev) => !prev)}
         />
         <MyPageButton
           text="링크 참여자 신청 시"
-          onPress={() => {
-            console.log('링크 참여자 신청 시');
-          }}
           type="toggle"
+          isToggleOn={linkJoinRequest}
+          onToggle={() => setLinkJoinRequest((prev) => !prev)}
         />
       </MarginContainer>
     </Container>
@@ -76,6 +79,7 @@ const Container = styled.View`
   background-color: ${colors.bg[2]};
   padding: ${28 * height}px ${20 * width}px;
 `;
+
 const MarginContainer = styled.View`
   margin-top: ${12 * height}px;
   gap: ${8 * height}px;
