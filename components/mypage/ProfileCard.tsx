1d92278809cc18e0727f0f7b77cf20b26bd2f5b8
JIN921 <wlslwlsl808@gmail.com>
2025-06-22 14:28:53 +0900

    style: #12 마이페이지 간격 수정

--- components/mypage/ProfileCard.tsx
+++ components/mypage/ProfileCard.tsx
@@ -66,7 +66,7 @@ const Container = styled.View`
   background-color: ${colors.white};
   width: ${336 * width}px;
   border-radius: ${radius.lg}px;
-  padding: ${24 * height}px ${20 * width}px;
+  padding: ${24 * height}px ${16 * width}px ${16 * height}px ${16 * width}px;
   align-items: center;
 `;
 
