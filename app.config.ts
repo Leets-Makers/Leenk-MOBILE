adaa03645e564ed2410cabbb974670739f6bd067
JIN921 <wlslwlsl808@gmail.com>
2025-07-01 14:22:28 +0900

    feat: #20 카카오 로그인 api 연결

---
+++ app.config.ts
@@ -0,0 +1,74 @@
+import 'dotenv/config';
+import { ExpoConfig, ConfigContext } from '@expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'Leenk',
+  slug: 'Leenk',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/ic_logo.png',
+  scheme: 'leenk',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  splash: {
+    image: './assets/images/ic_splash.png',
+    resizeMode: 'cover',
+    backgroundColor: '#F7F7FA',
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.example.leenk',
+    infoPlist: {
+      NSPhotoLibraryUsageDescription:
+        '사진을 선택하려면 접근 권한이 필요합니다.',
+    },
+  },
+  android: {
+    package: 'com.example.leenk',
+    edgeToEdgeEnabled: true,
+    adaptiveIcon: {
+      foregroundImage: './assets/images/ic_logo.png',
+      backgroundColor: '#ffffff',
+    },
+    permissions: [
+      'READ_MEDIA_IMAGES',
+      'READ_MEDIA_VIDEO',
+      'READ_EXTERNAL_STORAGE',
+      'WRITE_EXTERNAL_STORAGE',
+    ],
+  },
+  web: {
+    bundler: 'metro',
+    output: 'static',
+    favicon: './assets/images/ic_logo.png',
+  },
+  plugins: [
+    [
+      '@react-native-kakao/core',
+      {
+        nativeAppKey: process.env.KAKAO_NATIVE_APP_KEY,
+        android: {
+          authCodeHandlerActivity: true,
+        },
+        ios: {
+          handleKakaoOpenUrl: true,
+        },
+      },
+    ],
+    [
+      'expo-build-properties',
+      {
+        android: {
+          extraMavenRepos: [
+            'https://devrepo.kakao.com/nexus/content/groups/public/',
+          ],
+        },
+      },
+    ],
+    'expo-router',
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
 import { Image } from 'expo-image';
 import {
@@ -6,26 +6,106 @@ import {
   fontSize,
   height,
   lineHeight,
-  radius,
   width,
 } from '@/theme/globalStyles';
 import colors from '@/theme/color';
 import KakaoLogo from '@/assets/images/ic_KAKAO_symbol.svg';
 import { useRouter } from 'expo-router';
 import { CustomButton } from '@/components';
+import { initializeKakaoSDK } from '@react-native-kakao/core';
+import axios from 'axios';
+import { login } from '@react-native-kakao/user';
+import PopupModal from '@/components/Modal/PopupModal';
+import { Linking } from 'react-native';
 export default function LandingPage() {
+  const kakaoNativeAppKey = process.env.EXPO_PUBLIC_NATIVE_APP_KEY || '';
+  const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
   const router = useRouter();
+  const [notRegisterModal, setNotRegisterModal] = useState(false);
+  const [waitModal, setWaitModal] = useState(false);
+  const weethSiteURL = 'https://www.weeth.site/';
+  useEffect(() => {
+    initializeKakaoSDK(kakaoNativeAppKey);
+  }, []);
 
-  const handleKakaoLogin = () => {
-    console.log('카카오 로그인');
-    router.push('/signup/verify');
+  const handleKakaoLogin = async () => {
+    try {
+      // 1. 카카오 로그인 시도
+      const token = await login();
+      const accessToken = token?.accessToken;
+
+      console.log('✅ Kakao Access Token:', accessToken);
+
+      // 2. 서버에 토큰 전송
+      const response = await axios.post(
+        `${BASE_URL}kakao/login`,
+        {},
+        {
+          headers: {
+            'Kakao-Access-Token': accessToken,
+          },
+        },
+      );
+
+      console.log('로그인 성공: ', response.data);
+
+      // 3. 다음 화면으로 이동
+      router.push('/signup/verify');
+    } catch (error: any) {
+      if (axios.isAxiosError(error) && error.response?.data) {
+        const { code, message } = error.response.data;
+
+        switch (code) {
+          case '2000':
+            // 가입 승인 대기
+            setWaitModal(true);
+            break;
+          case '2001':
+            // 인증 서버 예외 메시지 → 콘솔 출력
+            console.error('서버 인증 에러:', message);
+            break;
+          case '2002':
+            // 미가입 사용자
+            setNotRegisterModal(true);
+            break;
+          default:
+            console.error('알 수 없는 예외:', code, message);
+        }
+      } else {
+        console.error('카카오 로그인 실패:', error);
+      }
+    }
   };
+
   const handleSignUp = () => {
+    Linking.openURL(weethSiteURL);
     console.log('새로 가입하기');
   };
 
   return (
     <Container>
+      <PopupModal
+        isOpen={notRegisterModal}
+        mainText="먼저 Weeth부터 가입해야 해"
+        subText="Leets 활동을 위해 위드는 필수야"
+        leftBtnText="닫기"
+        rightBtnText="위드 가입하자"
+        onConfirm={() => {
+          Linking.openURL(weethSiteURL);
+        }}
+        onClose={() => setNotRegisterModal(false)}
+      />
+      <PopupModal
+        isOpen={waitModal}
+        mainText="Weeth 가입 승인 대기중이야"
+        subText="승인이 완료될 때까지 조금만 기다려줘."
+        leftBtnText="닫기"
+        rightBtnText="위드 보러가자"
+        onConfirm={() => {
+          Linking.openURL(weethSiteURL);
+        }}
+        onClose={() => setWaitModal(false)}
+      />
       <LogoWrapper>
         <LogoGif
           source={require('@/assets/images/gif/ic_logo.gif')}
