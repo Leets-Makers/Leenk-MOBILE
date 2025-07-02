609c1ec6cc62773c20ee7360a44416dc13f59dd0
Dahyeon <dahhyeon22@gmail.com>
2025-07-02 22:08:08 +0900

    refactor: 정의된 상수값으로 패딩을 사용하도록 수정

--- app/(post)/feed.tsx
+++ app/(post)/feed.tsx
@@ -9,8 +9,7 @@ import { fontSize, fonts, height, width, radius } from '@/theme/globalStyles';
 import PopupModal from '@/components/Modal/PopupModal';
 import { useImageStore } from '@/stores/feedImageStore';
 import { AspectRatio } from '@/types/aspect-ratio';
-
-export const SIDE_PADDING = 16;
+import { CONTAINER_PADDING } from '@/constants';
 
 export default function PostFeedPage() {
   const selectedUrisLength = useImageStore(
@@ -88,7 +87,7 @@ export const Container = styled.View`
 
 export const ContentWrapper = styled.View`
   flex: 1;
-  padding: 0 ${SIDE_PADDING * width}px;
+  padding: 0 ${CONTAINER_PADDING * width}px;
 `;
 
 const SubText = styled.Text`
