657cb3276f8fc0a1849e987120b64cc3a512f7ba
Dahyeon <dahhyeon22@gmail.com>
2025-07-09 18:54:10 +0900

    feat: #25 내가 작성한/함께한 피드 목록 api연결

--- app/account/my-feed.tsx
+++ app/account/my-feed.tsx
@@ -1,5 +1,66 @@
-import LeenkPage from '@/app/(page)/leenk';
+import { Container } from '@/app/(post)/feed';
+import { FeedCard, Header, Loading } from '@/components';
+import TabMenu from '@/components/feed/TabMenu';
+import { FlatList, View } from 'react-native';
+import { useState } from 'react';
+import styled from 'styled-components/native';
+import { height } from '@/theme/globalStyles';
+import useFeedList from '@/hooks/useFeedList';
 
 export default function MyFeedPage() {
-  return <LeenkPage />;
+  const [tab, setTab] = useState<'uploaded' | 'joined'>('uploaded');
+
+  const {
+    data: feeds,
+    loadMore,
+    isLoading,
+    isRefreshing,
+    refresh,
+  } = useFeedList({
+    type: tab === 'uploaded' ? 'myJoined' : 'myFeed',
+    pageSize: 10,
+  });
+
+  if (feeds.length === 0 && isLoading) return <Loading />;
+  if (!feeds) return null;
+
+  return (
+    <Container>
+      <Header />
+      <TabMenu
+        activeTab={tab}
+        onTabChange={(newTab: string) => {
+          if (newTab === 'uploaded' || newTab === 'joined') {
+            setTab(newTab);
+            refresh(); // 탭 변경 시 새로고침
+          }
+        }}
+      />
+      <Content>
+        <FlatList
+          data={feeds}
+          numColumns={2}
+          keyExtractor={(item) => item.feedId.toString()}
+          columnWrapperStyle={{ justifyContent: 'space-between' }}
+          contentContainerStyle={{
+            paddingBottom: 100 * height,
+          }}
+          renderItem={({ item }) => <FeedCard item={item} />}
+          showsVerticalScrollIndicator={true}
+          onEndReached={loadMore}
+          onEndReachedThreshold={0.5}
+          refreshing={isRefreshing}
+          onRefresh={refresh}
+          ListFooterComponent={
+            feeds.length > 0 && isLoading ? <Loading /> : null
+          }
+        />
+      </Content>
+    </Container>
+  );
 }
+
+const Content = styled.View`
+  flex: 1;
+  margin-top: ${8 * height}px;
+`;
