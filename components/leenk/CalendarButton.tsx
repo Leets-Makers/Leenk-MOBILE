2723f436018e50e959c0bfb5c1f40cdef0fac8db
JIN921 <wlslwlsl808@gmail.com>
2025-07-30 02:24:15 +0900

    feat: #35 링크 모임 날짜 생성 버튼 구현

--- components/leenk/CalendarButton.tsx
+++ components/leenk/CalendarButton.tsx
@@ -1,3 +1,6 @@
+import React, { useState } from 'react';
+import { Platform, Pressable } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { CalendarIcon } from '@/assets';
 import colors from '@/theme/color';
 import {
@@ -9,31 +12,59 @@ import {
   width,
 } from '@/theme/globalStyles';
 import styled from 'styled-components/native';
+import dayjs from 'dayjs';
 
 export default function CalendarButton() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [showPicker, setShowPicker] = useState(false);
+
+  const handleChange = (_: any, date?: Date) => {
+    setShowPicker(false);
+    if (date) setSelectedDate(date);
+  };
+
   return (
-    <Container>
-      <StyledText>모임 일시를 선택해줘</StyledText>
-      <CalendarIcon />
-    </Container>
+    <>
+      <Container>
+        <StyledText selected={!!selectedDate}>
+          {selectedDate
+            ? dayjs(selectedDate).format('MM월 DD일 HH시')
+            : '모임 일시를 선택해줘'}
+        </StyledText>
+
+        <Pressable onPress={() => setShowPicker(true)}>
+          <CalendarIcon />
+        </Pressable>
+      </Container>
+
+      {showPicker && (
+        <DateTimePicker
+          value={selectedDate || new Date()}
+          mode="datetime"
+          display="default"
+          onChange={handleChange}
+          minimumDate={new Date()}
+        />
+      )}
+    </>
   );
 }
 
 const Container = styled.View`
   width: 100%;
   border-radius: ${radius.sm}px;
-  padding-vertical: ${12 * height}px;
-  padding-horizontal: ${12 * width}px;
+  padding: ${height * 12}px ${width * 12}px;
   border-width: 2px;
   border-color: ${colors.divider[2]};
-  border-style: solid;
   background-color: transparent;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-export const StyledText = styled.Text`
-  width: 100%;
+const StyledText = styled.Text<{ selected: boolean }>`
   font-size: ${fontSize.lg}px;
   line-height: ${lineHeight.l};
   font-family: ${fonts.Regular};
-  color: #cdced6;
+  color: ${({ selected }) => (selected ? colors.black : colors.text[3])};
 `;
