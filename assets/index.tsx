0d68ded447c2127cdb5ca55ac1976cccb1619d9b
nabbang6 <hawwah03@gachon.ac.kr>
2025-11-02 02:48:04 +0900

    design: 부가 기능 svg 교체

--- assets/index.tsx
+++ assets/index.tsx
@@ -38,6 +38,7 @@ import CheckerIcon from '@/assets/images/ic_checker_null.svg';
 import LocateIcon from '@/assets/images/ic_locate.svg';
 import ShareIcon from '@/assets/images/ic_share.svg';
 import ReviewIcon from '@/assets/images/ic_review.svg';
+import AdditionIcon from '@/assets/images/ic_navi_addition.svg';
 import LeenkImg01 from '@/assets/images/default_leenk/leenk_img_01.svg';
 import LeenkImg02 from '@/assets/images/default_leenk/leenk_img_02.svg';
 import LeenkImg03 from '@/assets/images/default_leenk/leenk_img_03.svg';
@@ -85,6 +86,7 @@ export {
   LocateIcon,
   ShareIcon,
   ReviewIcon,
+  AdditionIcon,
   LeenkImg01,
   LeenkImg02,
   LeenkImg03,
