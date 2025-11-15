e4ac3d7938e29b84e1ade2119b328f40d88111af
nabbang6 <hawwah03@gachon.ac.kr>
2025-11-16 03:28:02 +0900

    feat: 곧 생일자 카드 컴포넌트 추가

---
+++ components/private/UpcomingBirthdayCard.tsx
@@ -0,0 +1,68 @@
+import { ScrollView } from 'react-native';
+import ProfileImageWithFallback from '../feed/ProfileImageWithFallback';
+import { fonts, fontSize, height, radius, width } from '@/theme/globalStyles';
+import colors from '@/theme/color';
+import styled from 'styled-components/native';
+import { formatMonthDay } from '@/utils/format-date';
+
+export interface UpcomingBirthdayUser {
+  userId: number;
+  name: string;
+  thumbnail: string;
+  isUserBirthdayToday: boolean;
+  birthday: string;
+}
+
+export default function BirthdayCloseCardList({
+  data,
+}: {
+  data: UpcomingBirthdayUser[];
+}) {
+  return (
+    <ScrollContainer horizontal showsHorizontalScrollIndicator={false}>
+      {data.map((item, idx) => (
+        <Card
+          key={idx}
+          style={{
+            marginRight: idx === data.length - 1 ? 20 * width : 8, // 마지막 카드 마진
+            marginLeft: idx === 0 ? 20 * width : 0, // 첫 번째 카드 마진
+          }}
+        >
+          <ProfileImageWithFallback uri={item.thumbnail} size={40} />
+          <UserName>{item.name}</UserName>
+          <UserBirthday>{formatMonthDay(item.birthday)}</UserBirthday>
+        </Card>
+      ))}
+    </ScrollContainer>
+  );
+}
+
+const ScrollContainer = styled(ScrollView)`
+  width: 100%;
+`;
+
+const Card = styled.View`
+  width: ${95 * width}px;
+  height: ${124 * height}px;
+  padding: ${16 * height}px ${20 * width}px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.white};
+  border-radius: ${radius.lg}px;
+  margin-right: 8px;
+`;
+
+const UserName = styled.Text`
+  margin-top: 10px;
+  font-family: ${fonts.Regular};
+  font-size: ${fontSize['lg']}px;
+  color: ${colors.text[1]};
+`;
+
+const UserBirthday = styled.Text`
+  margin-top: 8px;
+  font-family: ${fonts.Regular};
+  font-size: ${fontSize['sm']}px;
+  color: ${colors.text[3]};
+`;
